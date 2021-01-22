a180229f1d9d6157b92e431b4135a724c65ba997
Ido Loven <idoloven123@gmail.com>
2021-01-22 03:37:25 +0200

    get_word_immediate() done

--- word_process.c
+++ word_process.c
@@ -15,8 +15,25 @@ word *get_first_word(OperationItem *command, int source, int dest){
 
 
 word *get_word_immediate(char *argument){
-    /* note that argument is a string - you need to process it  IDO*/
-
+    /* returns word that represents the immediate operand*/
+    char *operand;
+    word *immediate_word;
+    int int_operand;
+    if(is_comma(argument)){
+        drop_comma(argument);
+    }
+    
+    if(argument[1] == '-'){ /*in case operand is negative*/
+        strcpy(operand, &argument[2])
+        int_operand =-1 * atoi(operand);
+    }
+    else{
+        strcpy(operand, &argument[1]);
+        int_operand = atoi(operand);
+    }
+    
+    immediate_word->bits += int_operand
+    return immediate_word;
 }
 
 word *get_word_direct(char *argument){
