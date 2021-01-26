9bc40675e727420edb802f06379c7b54cd9d0c91
Amit Roth <amitroth@gmail.com>
2021-01-26 11:27:18 +0200

    change in structure

--- main.c
+++ main.c
@@ -4,5 +4,9 @@ int main(int argc, char *argv[]) {
     /* get the names from the command line as args
      * and send each one to assembler_main.c
      */
+    int i;
+    for (i=0; i++; i<argc){
+        assembler(argv[i]);
+    }
     return 0;
 }
